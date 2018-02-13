289b425c9956f6f2da02fb749f6c4ad80f6ca614
James C. Hurst <jchurstmail@gmail.com>
2018-02-13 09:58:56 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Created by JC Hurst
 
 [Github](https://github.com/jchurst) | [CodePen](http://codepen.io/jchurst/) | [LinkedIn](https://www.linkedin.com/in/jchurst) | [Webite](http://hurstcreative.com/) | [E-Mail](mailto:jchurstmail@gmail.com)
 
-# FLOATY - Determine material buoyancy
+# BOBBER - Determine material buoyancy
 
-• Determine the buoyancy of an object based on it’s displacement
-• Use the object's weight or density to determine the net buoyancy
+##### • Determine the buoyancy of an object based on it’s displacement
+##### • Use the object's weight or density to determine the net buoyancy
 
 [Project on Github](https://github.com/jchurst/floaty)
--- server.js
+++ server.js
@@ -3,23 +3,15 @@ const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
-require('./config/db');
 app.use('/', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.set('port', process.env.PORT || 8080);
-app.set('views', __dirname + '/views');
+app.set('data', __dirname + '/data');
 app.engine('html', handlebars({
-	extname: 'html',
-	defaultLayout: 'default',
-	layoutsDir: app.get('views') + '/layouts',
-	partialsDir: app.get('views') + '/partials'
+	extname: 'html'
 }));
 app.set('view engine', 'html');
-app.use(require('./routes/app.js'));
-app.use(require('./routes/errors.js'));
-io.on('connection', socket => require('./routes/io.js')(io, socket));
 http.listen(app.get('port'));
